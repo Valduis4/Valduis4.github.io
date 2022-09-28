495529c17bad53428aff6c4011adc2c20b3781cf
Valduis4 <samartherholt@gmail.com>
2022-09-28 07:44:51 -0600

    Index setup

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <!--<meta name=description content="This site was generated with Anima. www.animaapp.com"/>-->
+    <!-- <link rel="shortcut icon" type=image/png href="https://animaproject.s3.amazonaws.com/home/favicon.png" /> -->
+    <meta name="viewport" content="width=1440, maximum-scale=1.0" />
+    <link rel="shortcut icon" href="/images/favicon.ico">
+    <meta name="og:type" content="website" />
+    <meta name="twitter:card" content="photo" />
+    <link rel="stylesheet" type="text/css" href="./css/desktop-2.css" />
+    <link rel="stylesheet" type="text/css" href="./css/styleguide.css" />
+    <link rel="stylesheet" type="text/css" href="./css/globals.css" />
+    <link rel="stylesheet" href="/R64_Style.css?1">
+    <link rel="stylesheet" href="/topmenu/Top_Style.css?1">
+  </head>
+  <body style="margin: 0; background: #ffffff">
+  <script src="R64_Scripts.js?4"></script>
+
+    <input type="hidden" id="anPageName" name="page" value="desktop-2" />
+    <div class="container-center-horizontal">
+      <div class="desktop-2 screen">
+        <img class="image-1" src="./images/image-1@2x.png" alt="image 1" />
+        <img class="line-1" src="./images/line-1@1x.svg" alt="Line 1" />
+        <div class="frame-12">
+          <div class="frame-11">
+            <div class="frame-10">
+              <div class="analyze-millions-of">Generate instant product insight from 1 user to 1 million.</div>
+              <div class="understanding-what-y inter-normal-black-24px">
+                Understanding what your users do in your product requires understanding &amp; analyzing billions of data
+                points. Generate product insight and tell compelling stories to others with data, using Row64.
+              </div>
+            </div>
+            <div class="frame-18">
+              <div class="group-11">
+                <div class="overlap-group-3">
+                  <!-- <img
+                    class="screenshot-2022-09-22-at-1216-1"
+                    src="./images/screenshot-2022-09-22-at-12-16-1@1x.png"
+                    alt="Screenshot 2022-09-22 at 12.16 1"
+                  /> -->
+                    <video id="introVideo"muted playsinline autoplay loop poster="/images/Apple.png?2" style="border:0;height: 331px;left: 60px;object-fit: cover;position: absolute;top: 41px;width: 557px;">
+                        <source src="/videos/AppleNetSales.mp4" type=video/mp4>
+                    </video>
+                  <img
+                    class="apple-macbook-pro-16-2021-medium-1"
+                    src="./images/apple-macbook-pro-16-2021-medium-1@1x.png"
+                    alt="apple-macbook-pro-16-2021-medium 1"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="group-9">
+            <div class="overlap-group1">
+              <a href="/Register/" target="_blank"rel="noopener noreferrer">
+                <div class="sign-up-here inter-bold-white-30px">Sign Up Here</div>
+              </a>
+            </div>
+          </div>
+          <div class="frame-17">
+            <div class="im-amazed-at-the-s">
+              <span class="inter-bold-black-30px">“I&#39;m amazed at the speed - </span
+              ><span class="span1">a 20-minute Excel task is finished in seconds.</span
+              ><span class="inter-bold-black-30px">”</span><span class="inter-normal-black-30px">&nbsp;</span
+              ><span class="inter-normal-black-24px">- Associate Dean of Finance, PBS</span>
+            </div>
+          </div>
+          <div class="breakdown-your-big-d inter-bold-black-48px">
+            Breakdown your big data and use it to tell better stories
+          </div>
+        </div>
+        <div class="frame-15">
+          <div class="frame-1">
+            <div class="group-1">
+              <div class="overlap-group">
+                <img class="rectangle-3" src="./images/rectangle-3@1x.png" alt="Rectangle 3" />
+                <div class="big-data-big-insight-no-problem inter-bold-black-30px">
+                  Big data. Big insight. No problem.
+                </div>
+                <div class="speed-up-insight-ro inter-normal-black-24px">
+                  Speed up insight. Row64 processes billion points of data.
+                </div>
+                <div class="rectangle-4"></div>
+                <img class="image-2" src="./images/image-2@2x.png" alt="image 2" />
+              </div>
+            </div>
+            <div class="group-2">
+              <div class="overlap-group">
+                <img class="rectangle-3" src="./images/rectangle-3@1x.png" alt="Rectangle 3" />
+                <div class="build-a-custom-workflow-for-your-team inter-bold-black-30px">
+                  Build a custom workflow for your team
+                </div>
+                <div class="get-your-data-scient inter-normal-black-24px">
+                  Get your data scientist build a custom workflow for you. This way, data is shared across teams for
+                  better visibility and faster processes.
+                </div>
+                <div class="rectangle-4"></div>
+                <img class="image-2" src="./images/image-2-1@2x.png" alt="image 2" />
+              </div>
+            </div>
+          </div>
+          <div class="frame-1">
+            <div class="group-3">
+              <div class="overlap-group-4">
+                <img class="rectangle-3-1" src="./images/rectangle-3-2@1x.png" alt="Rectangle 3" />
+                <div class="make-strategic-business-decisions inter-bold-black-30px">
+                  Make strategic business decisions.
+                </div>
+                <div class="with-billions-of-dat inter-normal-black-24px">
+                With billions of data points received daily, Row64 help understand trends and behaviors on the spot. Convert all that data into actionable insights to optimize your business operations.
+                </div>
+                <div class="rectangle-4"></div>
+                <img class="image-2" src="./images/image-2-2@2x.png" alt="image 2" />
+              </div>
+            </div>
+            <div class="group-4">
+              <div class="overlap-group1-1">
+                <img class="rectangle-3-1" src="./images/rectangle-3-2@1x.png" alt="Rectangle 3" />
+                <div class="low-code-easy-to-use-tool inter-bold-black-30px">Low-code, easy-to-use tool</div>
+                <div class="row64-has-over-300-d inter-normal-black-24px">
+                  Row64 has over 300 data recipes to help process and tell engaging stories with your data- fast. So
+                  there’s no need to hire a programmer or pay for other apps.
+                </div>
+                <div class="rectangle-4"></div>
+                <img class="image-2" src="./images/image-2-3@2x.png" alt="image 2" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="overlap-group8">
+          <div class="group-8">
+            <div class="get-started-with-row64-in-3-easy-steps inter-bold-black-48px">
+              Get started with Row64 in 3 easy steps
+            </div>
+            <div class="overlap-group4">
+              <div class="step valign-text-middle inter-bold-white-24px">
+                STEP 1: Click the sign-up button to get started.
+              </div>
+            </div>
+            <div class="overlap-group-1">
+              <div class="step valign-text-middle inter-bold-white-24px">
+                STEP 2: Fill in the form with relevant details to unlock your trial period.
+              </div>
+            </div>
+            <div class="overlap-group-1">
+              <div class="step valign-text-middle inter-bold-white-24px">
+                STEP 3: Get details in your email to activate your account and get started.
+              </div>
+            </div>
+            <div class="overlap-group3">
+              <div class="designed-with-you-in-mind inter-bold-black-48px">Designed with you in mind</div>
+              <div class="sts-container inter-bold-black-30px">
+                <div class="business-analysts-yo">
+                  <span class="inter-bold-black-30px">Business Analysts<br /></span
+                  ><span class="inter-normal-black-24px"
+                    >You use spreadsheets, so you&#39;ll love the data recipes and geo-analysis charting.</span
+                  >
+                </div>
+                <div class="financial-analysts-y">
+                  <span class="inter-bold-black-30px">Financial<br />Analysts<br /></span
+                  ><span class="inter-normal-black-24px"
+                    >You are a big-picture kinda guy. Row64 gives you all that and more.</span
+                  >
+                </div>
+                <div class="data-scientists-from">
+                  <span class="inter-bold-black-30px">Data<br />Scientists<br /></span
+                  ><span class="inter-normal-black-24px"
+                    >From python data frames to GPU data investigation, Row64 just became your happy place.</span
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="overlap-group5"style="">
+              <a href="/Register/" target="_blank"
+                ><div class="sign-up-here inter-bold-white-30px">Sign Up Here</div>
+              </a>
+            </div>
+            <div class="overlap-group7"style="width:100%;flex-direction:column;">
+              <div class="data-thats-easy-to-digest inter-bold-black-48px" style="width:100%;max-width:950px;">Data that’s easy to digest</div>
+              <div class="frame" style="height:30px;background-color:#F7F8F9;max-width:950px">
+                <div class="container" style="background-color:#F7F8F9">
+                    <div class="PR_SwitchFill">
+                    <div class="PR_SwitchText" style="margin-left:auto;">Monthly</div>
+                    </div>
+                    <div class="PR_SwitchCenter">
+                    <input type="checkbox" id="switch" onclick="priceSwitch()"/><label for="switch" id="label" style="background-color:#AAAAAA;">Toggle</label>
+                    </div>
+                    <div class="PR_SwitchFill">
+                    <div class="PR_SwitchText">Yearly&nbsp;&nbsp;(Save up to 20%)</div>
+                    </div>
+                </div>
+            </div>
+            </div>
+          </div>
+          <div class="frame-16">
+            <div class="group-5">
+              <div class="flex-col">
+                <div class="flex-col-1">
+                  <div class="free inter-normal-black-24px">Free</div>
+                  <div class="flex-row">
+                    <div class="price inter-bold-black-30px">$0</div>
+                    <div class="monthly-per-user inter-normal-concord-14px">monthly per user</div>
+                  </div>
+                  <div class="overlap-group1-2">
+                    <div class="get-row64-free inter-bold-black-13px">Get Row64 Free</div>
+                    <div class="rectangle-12"></div>
+                  </div>
+                </div>
+                <div class="flex-row-1">
+                  <div class="icon-check_mark-container">
+                    <img class="icon-check_mark" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-1" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-2" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                  </div>
+                  <p class="non-commercial-use-a inter-normal-black-13px">
+                    Non-Commercial Use<br /><br />All Non-Enterprise Data<br />Features<br /><br />All Non-Enterprise
+                    Graphics<br />Features
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="group-6">
+              <div class="flex-col-2">
+                <div class="flex-col-3">
+                  <div class="flex-row-2"style="flex-direction: column;align-items: flex-start;">
+                  <div class="professional inter-normal-black-24px">Plus</div>
+                  <div class="flex-row-4">
+                    <div class="price-2 inter-bold-black-30px" id="PlusPrice">$25</div>
+                    <div class="monthly-per-user-2 inter-normal-concord-14px" id="PlusDetail">monthly per user</div>
+                  </div>
+                  </div>
+                  <div class="overlap-group-2" style="background: unset;flex-direction: column;align-items: flex-start;padding: 0;">
+                    <div class="PR_ButtonMonth" >
+                      <form action="/Register/" id="contactForm" method="POST">
+                        <button class="blueButton" name="plus" value="month">Subscribe Now</button>
+                      </form>
+                    </div>
+                    <div class="PR_ButtonYear">
+                      <form action="/Register/" id="contactForm" method="POST">
+                        <button class="blueButton" name="plus" value="year">Subscribe Now</button>
+                      </form>
+                    </div>
+                  </div>
+                  <p class="unlimited-commercial inter-normal-black-13px">
+                    Unlimited commercial use of the<br />following features:
+                  </p>
+                </div>
+                <div class="flex-row-3">
+                  <div class="icon-check_mark-container-1">
+                    <img class="icon-check_mark" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-1" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-2" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-2" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-2" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-1" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                  </div>
+                  <p class="high-speed-load-of-l inter-normal-black-13px">
+                    High speed load of large files<br /><br />GPU Paragraph Text Seach with Wildcards and Logic<br /><br />GPU
+                    Compute for primary data features<br /><br />Formulas that auto-generate Python code<br /><br />Chart
+                    Animation<br /><br />2 hours per month of support
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="group-7">
+              <div class="flex-col-5">
+                <div class="flex-col-6">
+                  <div class="overlap-group-5"><div class="most-popular">MOST POPULAR</div></div>
+                  <div class="professional inter-normal-black-24px">Professional</div>
+                  <div class="flex-row-4">
+                    <div class="price-2 inter-bold-black-30px" id="ProPrice">$100</div>
+                    <div class="monthly-per-user-2 inter-normal-concord-14px" id="ProDetail">monthly per user</div>
+                  </div>
+                  <div class="overlap-group-2" style="background: unset;flex-direction: column;align-items: flex-start;padding: 0;">
+                    <div class="PR_ButtonMonth" >
+                      <form action="/Register/" id="contactForm" method="POST">
+                        <button class="blueButton" name="pro" value="month">Subscribe Now</button>
+                      </form>
+                    </div>
+                    <div class="PR_ButtonYear">
+                      <form action="/Register/" id="contactForm" method="POST">
+                        <button class="blueButton" name="pro" value="year">Subscribe Now</button>
+                      </form>
+                    </div>
+                  </div>
+                  <p class="unlimited-commercial inter-normal-black-13px">
+                    Unlimited commercial use of the<br />following features:
+                  </p>
+                </div>
+                <div class="flex-row-5">
+                  <div class="icon-check_mark-container-2">
+                    <img class="icon-check_mark" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-1" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-1" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-1" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-2" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                    <img class="icon-check_mark-1" src="./images/image-3@2x.png" alt="icon-check_mark" />
+                  </div>
+                  <p class="everything-in-plus inter-bold-black-13px">
+                    <span class="inter-bold-black-13px">Everything in Plus, and...<br /></span
+                    ><span class="inter-normal-black-13px"
+                      ><br />Geoanalyis Features<br /><br />Natural Language<br /><br />Raytraced Presentation Charting
+                      &amp; Graphics<br /><br />Python Notebooks<br /><br />5 hours per month of support</span
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img class="icon-cursor" src="./images/group@2x.svg" alt="icon-cursor" />
+          <img class="vector" src="./images/group-1@2x.svg" alt="Vector" />
+          <img class="icon-mail" src="./images/vector@2x.svg" alt="icon-mail" />
+          <img class="group" src="./images/group-1@2x.svg" alt="Group" />
+          <img class="group-10" src="./images/group-2@2x.svg" alt="Group" />
+          <img class="icon-search" src="./images/group-4@2x.svg" alt="icon-search" />
+        </div>
+        <div class="copyright">© 2022 Row64</div>
+      </div>
+    </div>
+  </body>
+</html>
